bb53c00b9eb41121fdacd23286e8fc42b1e74575
RT1811 <ritwick.thebest@gmail.com>
2026-09-02 15:38:48 -0700

    Add published post detail endpoint

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -28,3 +28,36 @@ export async function getPublishedPosts(req, res, next) {
         next(err);
     }
 }
+
+export async function getPublishedPostById(req, res, next) {
+    try {
+        const post = await prisma.post.findFirst({
+            where: {
+                id: Number(req.params.id),
+                published: true,
+            },
+            select: {
+                id: true,
+                title: true,
+                content: true,
+                publishedAt: true,
+                author: {
+                    select: {
+                        id: true,
+                        username: true,
+                    },
+                },
+            },
+        });
+
+        if (!post) {
+            return res.status(404).json({
+                error: "Post not found",
+            });
+        }
+
+        res.status(200).json(post);
+    } catch(err) {
+        next(err);
+    }
+}
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { getPublishedPosts } from "../controllers/posts.js";
+import { getPublishedPosts, getPublishedPostById } from "../controllers/posts.js";
 
 const router = Router();
 
 router.get("/", getPublishedPosts);
+
+router.get("/:id", getPublishedPostById);
 
 export default router;
