97db834dfb5a204627bfe2312f41278597d4091e
RT1811 <ritwick.thebest@gmail.com>
2026-09-03 13:46:55 -0700

    Add user comment creation validation

--- server/controllers/comments.js
+++ server/controllers/comments.js
@@ -1,4 +1,5 @@
 import { prisma } from "../lib/prisma.js"
+import { validationResult } from "express-validator";
 
 export async function getPostComments(req, res, next) {
     const postId = Number(req.params.postId);
@@ -41,6 +42,61 @@ export async function getPostComments(req, res, next) {
         });
 
         res.status(200).json(comments);
+    } catch(err) {
+        next(err);
+    }
+}
+
+export async function createComment(req, res, next) {
+    const postId = Number(req.params.postId);
+    const { content } = req.body;
+
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.status(400).json({
+            errors: errors.array(),
+        });
+    }
+
+    try {
+        const post = await prisma.post.findFirst({
+            where: {
+                id: postId,
+                published: true,
+            },
+            select: {
+                id: true,
+            },
+        });
+
+        if(!post) {
+            return res.status(404).json({
+                error: "Post not found",
+            });
+        }
+
+        const comment = await prisma.comment.create({
+            data: {
+                content,
+                authorId: req.userId,
+                postId,
+            },
+            select: {
+                id: true,
+                content: true,
+                createdAt: true,
+                isUpdated: true,
+                author: {
+                    select: {
+                        id: true,
+                        username: true,
+                    },
+                },
+            },
+        });
+
+        res.status(201).json(comment);
     } catch(err) {
         next(err);
     }
--- server/routes/comments.js
+++ server/routes/comments.js
@@ -1,8 +1,17 @@
 import { Router } from "express";
 import { getPostComments } from "../controllers/comments.js";
+import { authenticate } from "../middleware/authentication.js"
 
 const router = Router();
 
+const commentValidation = [
+    body("content")
+        .trim()
+        .notEmpty()
+        .withMessage("Comment cannot be empty"),
+];
+
 router.get("/posts/:postId/comments", getPostComments);
+router.post("/posts/:postId/comments", authenticate, commentValidation, createComment);
 
 export default router;
