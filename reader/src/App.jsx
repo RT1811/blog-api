34dc49c92a413602c71e2057675066c5910fb095
RT1811 <ritwick.thebest@gmail.com>
2026-09-05 22:37:36 -0700

    Add logout for reader

--- reader/src/App.jsx
+++ reader/src/App.jsx
@@ -3,10 +3,13 @@ import Login from "./pages/Login.jsx";
 import Signup from "./pages/Signup.jsx";
 import Home from "./pages/Home.jsx";
 import Post from "./pages/Post.jsx";
+import Navbar from "./components/Navbar.jsx";
 
 function App() {
     return (
       <BrowserRouter>
+
+      <Navbar />
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
